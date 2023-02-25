56ff86ed704fc4c74d831f16881952cce55d3596
Neon <daniel@tecmie.com>
2023-02-25 16:12:14 +0100

    changed files names

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { WebhookService } from './webhook.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('WEBHOOK')
 @Controller('webhook')
 export class WebhookController {
   constructor(private readonly webhookService: WebhookService) {}
+
+  @Post()
+  handleWebhook(@Body() body: any) {
+    return this.webhookService.handleWebHook(body);
+  }
 }
