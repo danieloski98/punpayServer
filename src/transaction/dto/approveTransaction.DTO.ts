3851573901b1c8ae4ab1baff32802c3df755bd5f
daniel <danielemmanuel257@gmail.com>
2023-05-04 10:35:25 +0100

    added the user bank tto the transaction obect

--- src/transaction/dto/approveTransaction.DTO.ts
+++ src/transaction/dto/approveTransaction.DTO.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose } from 'class-transformer';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 @Exclude()
 export class ApproveTransactionDTO {
@@ -11,8 +11,11 @@ export class ApproveTransactionDTO {
   id: string;
 
   @IsString()
-  @IsNotEmpty()
+  @IsOptional()
   @Expose()
-  @ApiProperty()
+  @ApiProperty({
+    description:
+      'This is optional, this is ust for buy and swap transactions where the admin has to transfer crypto to the user',
+  })
   hash: string;
 }
